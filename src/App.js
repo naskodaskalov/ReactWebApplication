58d6542a13aceb8f318174f030e9bb18c5e0d56b
PetrovPetar <petrov90stz@gmail.com>
2016-11-29 16:15:58 +0200

    create Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavigationBar from './Components/NavigationBar';
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        username: sessionStorage.getItem("username"),
+        userId: sessionStorage.getItem("userId")
+    }
+  }
   render() {
     return (
       <div className="App">
+        <header>
+          <NavigationBar username={this.state.username}/>
+        </header>
       </div>
     );
   }
 }
 
-export default App;
---
+++ src/Components/NavigationBar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './NavigationBar.css';
+
+export default class NavigationBar extends Component {
+
+    render() {
+        if(this.props.username != null){
+            return (
+                <div className="links">
+                    <a href="#" >Home</a>
+                    <a href="#" >Posts</a>
+                    <a href="#" >Create Post</a>
+                    <a href="#" >Logout</a>
+                </div>
+            );
+        } else {
+            return (
+                <div className="links">
+                    <a href="#" >Home</a>
+                    <a href="#" >Login</a>
+                    <a href="#" >Register</a>
+                </div>
+            );
+        }
+
+    }
+}
