287ed63af013ce98831a498032d0967ccd78dc4e
Petar Stoyanov <pesho.stoyanov@gmail.com>
2016-12-02 23:48:11 +0200

    test commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+//test change comment - P.Stoyanov is trying to commit to project - feel free to delete it
 ReactDOM.render(
   <App />,
   document.getElementById('root')
